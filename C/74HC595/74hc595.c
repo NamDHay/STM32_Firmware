2b4a71507d7a87a06bb71ddf104513ee8f3b6326
NamDHay <nguyennamhuy2003@gmail.com>
2023-12-28 15:31:13 +0700

    Testing macro CONFIG_USE_*
    tại sao phải dùng macro  CONFIG_USE:
    	+ Thay vì mỗi lần gọi hàm HC595_* thì phải truyền đối tượng
    	*hc595, để rút gọn tham số *hc595 thì dùng một biến con trỏ toàn
    	cục ở bên trong file .c của thư viện, lúc này thư viện sẽ gọi
    	con trỏ nội bên trong để sử dụng
    	+ Do có một biến toàn cục trong thư viện, trong trường hợp thư
    	  viện không được sử dụng thì biến này là biến dư không dùng
    	  đến, dẫn tới chiếm dụng bộ nhớ không cần thiết
    	+ Do đó, CONFIG_USE_* dùng để loại bỏ nội dung thư viện không sử
    	  dụng bao gồm biến con trỏ toàn cục trong thư viện

---
+++ C/74HC595/74hc595.c
@@ -0,0 +1,28 @@
+/*
+ * 74hc595.c
+ *
+ *  Created on: Dec 28, 2023
+ *      Author: NamDHay
+ */
+#include "74hc595.h"
+
+void HC595_AssignPin(HC595PIN *hc595, GPIO_TypeDef *port, uint16_t pin, pinName name){
+    switch(name){
+        case HC595_DS:
+        dsport = port;
+        dspin = pin;
+        break;
+        case HC595_CLK:
+        clkport = port;
+        clkpin = pin;
+        break;
+        case HC595_LATCH:
+        latchport = port;
+        latchpin = pin;
+        break;
+        case HC595_OE:
+        oeport = port;
+        oepin = pin;
+        break;
+    }
+}
---
+++ C/74HC595/74hc595.h
@@ -0,0 +1,54 @@
+/*
+ * 74hc595.h
+ *
+ *  Created on: Dec 28, 2023
+ *      Author: NamDHay
+ */
+
+#ifndef INC_74HC595_H_
+#define INC_74HC595_H_
+
+#include "main.h"
+#ifdef CONFIG_USE_74HC595
+#include "string.h"
+#include "stdbool.h"
+
+#define dspin                   hc595->ds.pin
+#define dsport                  hc595->ds.port
+#define clkpin                  hc595->clk.pin
+#define clkport                 hc595->clk.port
+#define latchpin                hc595->latch.pin
+#define latchport               hc595->latch.port
+#define oepin                   hc595->oe.pin
+#define oeport                  hc595->oe.port
+
+#define DELAY_MS(time)      (HAL_Delay(time))
+#define HC595_WRITE(PIN, LOGIC) ((PIN == HC595_DS) ? HAL_GPIO_WritePin(latchport, latchpin, LOGIC)      :
+                                ((PIN == HC595_CLK) ? HAL_GPIO_WritePin(clkport, clkpin, LOGIC))        :
+                                ((PIN == HC595_LATCH) ? HAL_GPIO_WritePin(latchport, latchpin, LOGIC))  :
+                                ((PIN == HC595_OE) ? HAL_GPIO_WritePin(oeport, oepin, LOGIC))           : 0)
+
+
+typedef struct{
+    GPIO_TypeDef *port;
+    uint16_t pin;
+}pinConfig;
+
+typedef struct{
+    pinConfig ds;
+    pinConfig clk;
+    pinConfig latch;
+    pinConfig oe;
+}HC595PIN;
+
+typedef enum{
+    HC595_DS,
+    HC595_CLK,
+    HC595_LATCH,
+    HC595_OE,
+}pinName;
+
+void HC595_AssignPin(HC595PIN *hc595, GPIO_TypeDef *port, uint16_t pin, pinName name);
+void HC595_ShiftOut()
+#endif /* INC_74HC595_H_ */
+#endif
