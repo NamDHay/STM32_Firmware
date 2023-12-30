75a625a01137d5ae82c6e423f012ab84a58db9ed
NamDHay <nguyennamhuy2003@gmail.com>
2023-12-30 17:24:15 +0700

    Update hc595.cpp

--- C++/74HC595/74hc595.h
+++ C++/74HC595/74hc595.h
@@ -19,29 +19,37 @@
                                 ((PIN == HC595_LATCH) ? HAL_GPIO_WritePin(latchport, latchpin, LOGIC))  :
                                 ((PIN == HC595_OE) ? HAL_GPIO_WritePin(oeport, oepin, LOGIC))           : 0)
 
+typedef enum{
+	HC595_LATCH,
+	HC595_CLK,
+	HC595_DS,
+	HC595_OE
+}pinName;
+
+typedef enum{
+	LSB,
+	MSB,
+}ShiftType;
+
 class HC595{
 private:
     typedef struct pinConfig{
 	    GPIO_TypeDef *port;
 	    uint16_t pin;
     }pinConfig;
-    typedef enum{
-	    HC595_LATCH,
-	    HC595_CLK,
-	    HC595_DS,
-	    HC595_OE
-    }pinName;
     typedef struct HC595{
 	    pinConfig ds;
 	    pinConfig clk;
 	    pinConfig latch;
 	    pinConfig oe;
-		uint32_t data;
 	}HC595pin;
 	HC595pin hc595pin;
+	uint32_t data;
 public:
     void AssignPin(GPIO_TypeDef *port, uint16_t pin, pinName name);
-	void ShiftOut();
+	void ShiftOut(uint8_t n, ShiftType type);
+	void SetBit(uint8_t bit, uint8_t n);
+	void ClearBit(uint8_t bit, uint8_t n);
 }
 
 
