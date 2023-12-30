25190f1e575ad14fa08b794258d39c103a380038
NamDHay <nguyennamhuy2003@gmail.com>
2023-12-30 07:55:24 +0700

    Add LCD::Write

--- Core/Inc/lcdi2c.h
+++ Core/Inc/lcdi2c.h
@@ -11,22 +11,71 @@
 #include "main.h"
 #include "string.h"
 
+#define LCD_CLEARDISPLAY 				0x01
+#define LCD_RETURNHOME 					0x02
+
+#define LCD_ENTRYMODESET 				0x04
+#define LCD_DISPLAYCONTROL 				0x08
+#define LCD_CURSORSHIFT 				0x10
+#define LCD_FUNCTIONSET 				0x20
+#define LCD_SETCGRAMADDR 				0x40
+#define LCD_SETDDRAMADDR	 			0x80
+
+// flags for display entry mode
+#define LCD_ENTRYRIGHT 					0x00
+#define LCD_ENTRYLEFT 					0x02
+#define LCD_ENTRYSHIFTINCREMENT 		0x01
+#define LCD_ENTRYSHIFTDECREMENT 		0x00
+
+// bits for display on/off control
+#define LCD_DISPLAY						2
+#define LCD_CURSOR						1
+#define LCD_BLINK 						0
+// flags for display/cursor shift
+#define LCD_DISPLAYMOVE 				0x08
+#define LCD_CURSORMOVE 					0x00
+
+#define LCD_MOVERIGHT 					0x04
+#define LCD_MOVELEFT 					0x00
+
+// flags for function set
+#define LCD_8BITMODE 					0x10
+#define LCD_4BITMODE 					0x00
+#define LCD_2LINE 						0x08
+#define LCD_1LINE 						0x00
+#define LCD_5x10DOTS 					0x04
+#define LCD_5x8DOTS 					0x00
+
 #define LCD_Delay(time)	HAL_Delay(time)
+
+typedef enum{
+	LCD_COMMAND,
+	LCD_DATA,
+}LCDmode;
+
+typedef enum{
+	OFF,
+	ON,
+}State;
 
 class LCD{
 private:
 	typedef struct{
-		I2C_HandleTypeDef* i2c;
-		uint8_t row;
-		uint8_t column;
-		uint8_t addr;
 		uint8_t entrymode;
 		uint8_t displayctrl;
 		uint8_t cursorshift;
 		uint8_t functionset;
 		uint8_t backlight;
+	}LCDCmd;
+	typedef struct{
+		I2C_HandleTypeDef* i2c;
+		uint8_t row;
+		uint8_t column;
+		uint8_t addr;
+		LCDCmd cmd;
 	}LCD_I2C;
 	LCD_I2C lcd;
+	void Write(uint8_t data, LCDmode mode);
 public:
 	void Init(I2C_HandleTypeDef* hi2c, uint8_t address, uint8_t column, uint8_t row);
 };
--- Core/Src/lcdi2c.cpp
+++ Core/Src/lcdi2c.cpp
@@ -6,10 +6,21 @@
  */
 
 #include "lcdi2c.h"
-
+void LCD::Write(uint8_t data, LCDmode mode){
+	char Data_H;
+	char Data_L;
+	uint8_t Data_I2C[4];
+	Data_H = data&0xF0;
+	Data_L = (data<<4)&0xF0;
+}
 void LCD::Init(I2C_HandleTypeDef* hi2c, uint8_t address, uint8_t column, uint8_t row){
 	lcd.i2c = hi2c;
 	lcd.addr = address;
 	lcd.row = row;
 	lcd.column = column;
+
+	lcd.cmd.functionset = LCD_FUNCTIONSET|LCD_4BITMODE|LCD_2LINE|LCD_5x8DOTS;
+	lcd.cmd.entrymode = LCD_ENTRYMODESET|LCD_ENTRYLEFT|LCD_ENTRYSHIFTDECREMENT;
+	lcd.cmd.displayctrl = LCD_DISPLAYCONTROL|(1<<LCD_DISPLAY)|(0<<LCD_CURSOR)|(0<<LCD_BLINK);
+	lcd.cmd.cursorshift = LCD_CURSORSHIFT|LCD_CURSORMOVE|LCD_MOVERIGHT;
 }
